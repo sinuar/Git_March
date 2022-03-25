998fa24c9eb605c54ecbdcce1b25db05b6f1a200
Sinuhé Ruedas <sinuar@Sinuars-MacBook-Pro.local>
2022-03-25 12:05:29 -0600

    Creation of Bitcoin project

---
+++ Bitcoin_project/Bitcoin_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bitcoin_project
+//
+//  Created by Sinuhé Ruedas on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
